4a9d19cc4c2a3145223544d192ab3f550f7dd3ef
Chris Kay <chris@cjkay.uk>
2016-04-07 20:45:47 +0100

    Add result and argument types

--- examples/check/check.c
+++ examples/check/check.c
@@ -4,6 +4,14 @@
  * obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <cordic.h>
+
+struct cordic_result example_fail_check(struct cordic_args args) {
+    (void)args;
+
+    return (struct cordic_result) { 42 };
+}
+
 int main(void) {
     return 0;
 }
