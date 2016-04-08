9be24601b7fff07051c6e637d0c3d49abb5c1b42
Chris Kay <chris@cjkay.uk>
2016-04-08 09:41:28 +0100

    Reserve cordic_args for future use

--- examples/check/check.c
+++ examples/check/check.c
@@ -10,15 +10,11 @@
 #include <stdio.h>
 #endif
 
-struct cordic_result example_success(struct cordic_args args) {
-    (void)args;
-
+struct cordic_result example_success(cordic_args) {
     cordic_success;
 }
 
-struct cordic_result example_failure(struct cordic_args args) {
-    (void)args;
-
+struct cordic_result example_failure(cordic_args) {
     cordic_assert(1 == 1);
     cordic_assert(1 == 2, "1 does not equal 2!");
 
@@ -27,13 +23,11 @@ struct cordic_result example_failure(struct cordic_args args) {
 
 #ifdef __STDC_HOSTED__
 int main(void) {
-    struct cordic_args args = { 0 };
-
-    struct cordic_result success = example_success(args);
+    struct cordic_result success = example_success();
     printf("%d, %p, %p, %p\n", success.line, (void *)success.file,
         (void *)success.fn, (void *)success.msg);
 
-    struct cordic_result failure = example_failure(args);
+    struct cordic_result failure = example_failure();
     printf("%d, \"%s\", \"%s\", \"%s\"\n", failure.line, failure.file,
         failure.fn, failure.msg);
 
